777926ffb03fb477dcae935067f94831e1194cfb
Stibbons <gaetan@xeberon.net>
2013-11-21 11:46:15 +0100

    Create string.rs

---
+++ string.rs
@@ -0,0 +1,27 @@
+
+use std::ascii::StrAsciiExt;
+
+fn to_lower_case(s: ~str)
+{
+  return ??
+}
+
+fn to_upper_case(s: ~str)
+{
+  return ??
+}
+
+fn to_title_case(s: ~str)
+{
+  return ??
+}
+
+fn to_camel_case(s: ~str)
+{
+  return ??
+}
+
+fn to_underscore_case(s: ~str)
+{
+  return ??
+}
